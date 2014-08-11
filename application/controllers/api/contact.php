408a56360ea0df482c818ea3ac0de5e216db1608
williecheong <cheongwillie@hotmail.com>
2014-08-11 01:38:54 -0400

    rough edit of email contact api

--- application/controllers/api/contact.php
+++ application/controllers/api/contact.php
@@ -14,41 +14,30 @@ class Contact extends REST_Controller {
 
     // Used to create a new group in the DB
     public function index_post() {
-        $data = clean_input( $this->post() );
-        
         if ( isset($data['message']) ){
             $email = '';
             if ( isset($data['email']) ) {
                 $email = $data['email'];
-            } 
-
-            $user_id = 0;
-            if ( $this->session->userdata('user_id') ) {
-                $user_id = $this->session->userdata('user_id');
-            }
-
-            $feedback_id = $this->feedback->create(
-                array(
-                    'email' => $email,
-                    'user_id' => $user_id,
-                    'ip_address' => $_SERVER['REMOTE_ADDR'],
-                    'message' => $data['message']
-                )
-            );
-
-            echo json_encode(
-                array(
-                    'status' => 'success',
-                    'message' => 'Feedback posted successful',
-                    'feedback_id' => $feedback_id
-                )
-            );
             
+                echo json_encode(
+                    array(
+                        'status' => 'success',
+                        'message' => 'Email message successfully sent'
+                    )
+                );
+            } else {
+                echo json_encode(
+                    array(
+                        'status' => 'fail',
+                        'message' => 'Sender email address not specified'
+                    )
+                );
+            }
         } else {
             echo json_encode( 
                 array(
                     'status'  => 'fail',
-                    'message' => 'Message is missing from feedback'
+                    'message' => 'Message is missing from email'
                 )
             );
         }
